ece09ce10639b2e86e8137dfe6bdf5b7de580511
Randomguy01 <ianhwhite2001@gmail.com>
2021-11-14 17:31:32 -0500

    Created Contact to represent another user
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

---
+++ lib/src/messaging/screens/new_conversation/models/contact.dart
@@ -0,0 +1,32 @@
+
+/// Represents other users the current user can start a conversation with.
+class Contact {
+
+  /// The name of the contact.
+  final String name;
+
+  /// The university/college email of that user.
+  final String email;
+
+  /// The floor the user lives on
+  final String floor;
+
+  /// Default constructor requires all parameters.
+  Contact({
+    required this.name,
+    required this.email,
+    required this.floor,
+  });
+
+  /// Returns a one or two letter initials for the name of the user.
+  String get initials {
+    final nameParts = name.split(' ');
+
+    assert(nameParts.isNotEmpty);
+    if (nameParts.length == 1) {
+      return nameParts[0][0];
+    } else {
+      return nameParts[0][0] + nameParts[1][0];
+    }
+  }
+}
