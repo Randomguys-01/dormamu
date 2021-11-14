c581f62644d3233b335ecf3153722c0c1fc65d84
Randomguy01 <ianhwhite2001@gmail.com>
2021-11-14 17:32:51 -0500

    contact_list.dart displays all contacts the user can start a conversation with
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

---
+++ lib/src/messaging/screens/new_conversation/widgets/contact_list.dart
@@ -0,0 +1,29 @@
+import 'package:dormamu/src/messaging/screens/new_conversation/models/contact.dart';
+import 'package:flutter/material.dart';
+
+import 'contact_tile.dart';
+
+/// Custom [ListView] displays all [Contact]s that the current user can start a
+/// conversation with.
+class ContactList extends StatelessWidget {
+  ContactList({
+    Key? key,
+    required List<Contact> contacts,
+  })  : _contacts = contacts
+          ..sort((contact1, contact2) {
+            return contact1.name.compareTo(contact2.name);
+          }),
+        super(key: key);
+
+  /// The [List] of [Contact]s being displayed.
+  final List<Contact> _contacts;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: _contacts.length,
+        itemBuilder: (_, int index) {
+          return ContactTile(contact: _contacts[index]);
+        });
+  }
+}
