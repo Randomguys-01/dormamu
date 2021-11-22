b6cfc9ec06a26fff1d77208b93cab9abddf028f7
Randomguy01 <ianhwhite2001@gmail.com>
2021-11-21 21:49:25 -0500

    Created tabs to display events by type
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

--- lib/src/events/events.dart
+++ lib/src/events/events.dart
@@ -1,12 +1,33 @@
+import 'package:dormamu/src/events/widgets/events_tab.dart';
 import 'package:flutter/material.dart';
+
+import 'models/event.dart';
 
 class EventsDestination extends StatelessWidget {
   const EventsDestination({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Events')),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Events'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Community'),
+              Tab(text: 'Private'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            EventTab(eventType: EventType.community),
+            EventTab(eventType: EventType.private),
+          ],
+        ),
+      ),
     );
   }
 }
