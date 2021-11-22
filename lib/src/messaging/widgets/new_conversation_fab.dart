4c2a042577271e120f0398776e58e90428a16017
Randomguy01 <ianhwhite2001@gmail.com>
2021-11-21 20:14:51 -0500

    Launches the NewConversationScreen
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

--- lib/src/messaging/widgets/new_conversation_fab.dart
+++ lib/src/messaging/widgets/new_conversation_fab.dart
@@ -1,18 +1,24 @@
+import 'package:dormamu/src/messaging/models/conversation_list_state.dart';
 import 'package:dormamu/src/messaging/screens/new_conversation/new_conversation_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NewConversationFAB extends StatelessWidget {
   const NewConversationFAB({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final conversationListState = Provider.of<ConversationListState>(context, listen: false);
     return FloatingActionButton(
       child: const Icon(Icons.add),
       onPressed: () {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (_) => const NewConversationScreen(),
+            builder: (_) => ChangeNotifierProvider.value(
+              value: conversationListState,
+              child: const NewConversationScreen(),
+            ),
           ),
         );
       },
