f9e4fc239bfe83b54e20453eb93c94ce6a077952
Randomguy01 <ianhwhite2001@gmail.com>
2021-12-04 23:16:29 -0500

    Created NewEventScreen to create a new Event
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

---
+++ lib/src/screens/new_event/new_event_screen.dart
@@ -0,0 +1,41 @@
+import 'package:dormamu/src/events/models/event_list_state.dart';
+import 'package:dormamu/src/screens/new_event/widgets/create_event_fab.dart';
+import 'package:dormamu/src/screens/new_event/widgets/new_event_form.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/new_event_form_state.dart';
+
+/// Allows the user to create a new [Event] via the [NewEventForm].
+class NewEventScreen extends StatelessWidget {
+  /// Default constructor
+  const NewEventScreen({
+    Key? key,
+    required this.eventListState,
+  }) : super(key: key);
+
+  /// Used to add the new [Event] to the list of events.
+  final EventListState eventListState;
+
+  @override
+  Widget build(_) {
+    // Create key to control NewEventForm
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+    // Create form state
+    return ChangeNotifierProvider(
+        create: (_) => NewEventFormState(),
+        builder: (BuildContext context, _) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('New Event')),
+            body: NewEventForm(
+              formKey: formKey,
+            ),
+            floatingActionButton: CreateEventFAB(
+                formKey: formKey, eventListState: eventListState),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
+        });
+  }
+}
