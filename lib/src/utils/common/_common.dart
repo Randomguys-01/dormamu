6f88b7b42c725635dfb31d5c6c710b4dc1878a7b
Randomguy01 <ianhwhite2001@gmail.com>
2022-02-24 19:25:06 -0500

    Created common for top-level functions; Create date_format for formatting DataTimes
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

---
+++ lib/src/utils/common/_common.dart
@@ -0,0 +1,2 @@
+/// Barrel file for common, top-level functions and properties
+export 'date_format.dart';
---
+++ lib/src/utils/common/date_format.dart
@@ -0,0 +1,4 @@
+/// Contains top-level functions and properties for formatting [DateTime].
+
+/// The full date and time written out. Ex: February 24 2022, 6:54 PM
+const String kFormalDateTime = "MMMM d yyyy, h:m a";
